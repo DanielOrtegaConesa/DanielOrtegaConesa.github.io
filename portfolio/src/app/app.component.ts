7957e52d241ecdf9440479658e50bed8d144cf59
Daniel Ortega Conesa <dortega@siptize.com>
2020-05-14 18:27:22 +0200

    cambio layout

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -6,22 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public title = 'portfolio';
   public config: any;
   public fullpageApi: any;
 
   constructor() {
 
-    // for more details on config options please visit fullPage.js docs
     this.config = {
-      // fullpage options
       licenseKey: 'HIesNLH!b3',
-      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      anchors: ['home', 'technologies'],
       menu: '#menu',
-      // sectionsColor: ['#ccc', '#ccc', '#ccc', '#ccc', '#ccc'],
-      // fullpage callbacks
+      navigation: true,
+      slidesNavigation: true,
+      controlArrows: false,
       afterResize: () => {
-        console.log("After resize");
       },
       afterLoad: (origin, destination, direction) => {
         console.log(origin.index);
